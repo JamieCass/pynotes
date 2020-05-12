db8b38e06e66dbe11e88c5b81ea6d48735c77311
Jamie <jamiecass92@gmail.com>
2020-05-12 19:19:07 +1000

    more on OOP, __init__ method

--- oop.py
+++ oop.py
@@ -69,6 +69,64 @@ boat = Vehicle()
 
 
 
+##########################################
+# the __init__ method
+##########################################
+
+#Everytime we make a class python we need to define the __init__ method unless there no data in the class (rare).
+
+#Always have to have self at the beginning of __init__. "def __init__(self):"
+
+#Self keyword refers to the current class instance.
+
+#Technically it doesnt have to be called self but its standard and its pretty much the only thing you will see.
+#self pretty much calls the instance. so below its just caling user then we define first and second and so on in brackets.
+
+# A User class with 3 attributes but no methods (aside from __init__)
+class User:
+	def __init__(self, first, last, age):
+		self.first = first #this is setting the first name attribute to 'Joe' for ser 1 and 'blanca' for user 2.
+		self.last = last  #same as above but for the last name.
+		self.age = age  #again the same this as above.
+
+user1 = User("Joe","Smith", 68)
+user2 = User("Blanca", "Lopez", 41)
+
+#when we want to print it, we have to print what we've called it in the self section.
+#so it will be first, last, age. (all 3 are in called from self.first, last, or age.)
+
+print(user1.first, user1.last, user1.age)
+print(user2.first, user2.last, user2.age)
+
+# e.g. 2 
+
+class Vehicle:
+
+	def __init__ (self, make, model, year):
+		self.make = make
+		self.model = model
+		self.year = year
+
+#creating an object that is an instance of a class is called INSTANTIATING a class.
+# e.g. 
+
+v = Vehicle('Honda','Civic', 2017)
+# v becomes a Honda Civic, a new instance of Vehicle.
+
+# v
+# <__main__.Vehicle at 0x10472f5c0>
+# v.make
+# 'Honda'
+# v.model
+# 'Civic'
+# v.year
+# 2017
+
+
+
+
+
+
 
 
 
