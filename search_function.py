2e1e9659fe67e4e0818672d0af6f60f615aee255
Jamie <jamiecass92@gmail.com>
2020-05-19 18:47:57 +1000

    changed the search function, actually works now

--- search_function.py
+++ search_function.py
@@ -1,10 +1,23 @@
-fileName = input("Paste file name here")
+# fileName = input("Paste file name here")
+#
+# with open(fileName) as inputFile:
+#     data = inputFile.readlines()
+#     inputFile.close()
+#
+# for i, line in enumerate(data):
+#     searchPhrase1 = "d = Deck()"
+#     if searchPhrase1 in line:
+#         return line
 
-with open(fileName) as inputFile:
-    data = inputFile.readlines()
-    inputFile.close()
 
-for i, line in enumerate(data):
-    searchPhrase1 = "d = Deck()"
-    if searchPhrase1 in line:
-        return line
+def check(lookup):
+    with open('cards.py') as f:
+        datafile = f.readlines()
+    #found = False  # This isn't really necessary
+    for line in datafile:
+        if lookup.lower() in line:
+            # found = True # Not necessary
+            return True
+    return False  # Because you finished the search without finding
+
+check('cards')
