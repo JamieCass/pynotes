346a4527e46b9e4936824fd845a9052146b925a1
Jamie <jamiecass92@gmail.com>
2020-05-24 14:50:31 +1000

    added a bit on iterators, needs comments adding to it

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![atl text](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTpqzgPni981kXAqtY12cHp_iO0D-synY2O8AEYGrjSr77rIc0o&usqp=CAU)
-# pynotes
+# PYNOTES
 
 
 ### Some helpful notes and a few functions that have helped me get to grips with python.
--- iterators_generators.py
+++ iterators_generators.py
@@ -17,5 +17,46 @@
 
 
 ##########################################
-#
+# Custom For Loop
 ##########################################
+#Ill go through this and make comments when tomorrow!!!
+# for num in [1,2,3]
+# for char in "hi there"
+
+def my_for(iterable, func):
+	iterator = iter(iterable)
+	while True:
+		try:
+			thing = next(iterator)
+		except StopIteration:
+			break
+		else:
+			func(thing)
+
+def square(x):
+	print(x*x)
+
+my_for("lol", print)
+my_for([1,2,3,4,5], square)
+
+##########################################
+class Counter:
+	def __init__(self, low, high):
+		self.current = low
+		self.high = high
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		if self.current < self.high:
+			num = self.current
+			self.current += 1
+			return num
+		raise StopIteration
+
+
+
+
+for x in Counter(50,70):
+	print(x)
