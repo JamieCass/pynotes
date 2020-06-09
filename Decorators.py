a214aa164f7df9cfd51c332544d01de7d14cf1db
Jamie <jamiecass92@gmail.com>
2020-06-09 17:49:40 +1000

    added to decoratos

--- Decorators.py
+++ Decorators.py
@@ -1,3 +1,162 @@
 #######################################################################
 # Decorators
 #######################################################################
+##########################################
+#Higher order functions (prep for decorators.
+##########################################
+#wont really use this but its useful to know., and it helps with decorators...
+#we can nest functions inside one another.. this is a good example
+from random import choice
+
+def greet(person):
+    def get_mood():
+        msg = choice(('Hello there ', 'Go away ', 'I love you '))
+        return msg
+    #this will only return a print statment pretty much.
+    result = get_mood() + person
+    return result
+
+greet('Jamie')
+
+#we can have a function return a function.
+def make_laugh_func():
+    def get_laugh():
+        l = choice(('HAHAHA', 'lol', 'tehehe'))
+        return l
+
+    return get_laugh
+
+laugh = make_laugh_func()
+print(laugh())
+
+##########################################
+#inner functions can access outer function scope
+def make_laugh_at_func(person):
+    def get_laugh():
+        laugh = choice(('HAHAHA', 'lol', 'tehehe'))
+        #person is in the outer function not in the inner function.
+        return f'{laugh} {person}'
+
+    return get_laugh
+
+laugh_at = make_laugh_at_func('Sophie')
+laugh_at()
+
+##########################################
+#What are Decorators????
+##########################################
+#decorators are Functions
+#the wrap other functions and enhance their behaviour
+#they are examples of higher order functions
+#they have their own syntax, using '@'(syntax sugar)
+
+def be_polite(fn):
+    def wrapper():
+        print('What a pleasure to meet you!')
+        fn()
+        print('Have a great day!')
+    return wrapper
+#we are decorating our function with politeness!
+#we are 'wrapping' the above funciton in with the function below!
+# def greet():
+#     print('My name is Jamie.')
+####-------------------------------NEW CODE----------------------------------------------####
+#we can use a decorator to do the same thing as above!!.
+@ be_polite
+def greet():
+    print('My name is Sophie')
+@be_polite
+def rage():
+    print('I hate you!!')
+#this means we dont need to set what greet is!
+#greet = be_polite(greet)
+####-------------------------------------------------------------------------------------####
+
+#because we added thenew code.. we can just call greet sratight away.. (without having to set greet)
+greet()
+rage()
+
+
+##########################################
+#Functions with different signatures
+##########################################
+
+# def shout(fn):
+#     def wrapper(name):
+#         return fn(name).upper()
+#     return wrapper
+
+####--------------------------------NEW CODE---------------------------------------------####
+## with the *args and **kwargs we can make anything work with with the shout function.
+def shout(fn):
+    def wrapper(*args, **kwargs):
+        return fn(*args, *kwargs).upper()
+    return wrapper
+##this means we can add anything to shout and it doesnt matter how many args or kwargs we have it will still work.
+####-------------------------------------------------------------------------------------####
+
+@shout
+def greet(name):
+    return f'Hi ,im {name}'
+
+#this wont work in the old code, see what ive added to make it work. (it has two arguments, the wrapper in shout only has 1 arguments.)
+@shout
+def order(main, side):
+    return f"Hi, I'd like the {main}, with a side or {side} please."
+
+greet('Jamie')
+order('Steak', 'Fries')
+
+
+##########################################
+#Using wraps to preserve metadata
+##########################################
+
+# def log_function_data(fn):
+#     def wrapper(*args, **kwargs):
+#         '''I AM A WRAPPER FUNCTION'''
+#         print(f'You are about to call {fn.__name__}')
+#         print(f'Here is the documentation: {fn.__doc__}')
+#         return fn(*args, **kwargs)
+#     return wrapper
+# @log_function_data
+# def add(x,y):
+#     '''Adds two numbers together'''
+#     return x+y
+# add(3,4)
+#
+# add.__doc__ #wont work properly it will just come up with the wrapper documentation
+
+from functools import wraps
+def log_function_data(fn):
+    ####--------------------------------NEW CODE---------------------------------------------####
+    @wraps(fn) #this will let us keep the doc for add and also be able to call help on it as well.
+    ####-------------------------------------------------------------------------------------####
+    def wrapper(*args, **kwargs):
+        '''I AM A WRAPPER FUNCTION'''
+        print(f'You are about to call {fn.__name__}')
+        print(f'Here is the documentation: {fn.__doc__}')
+        return fn(*args, **kwargs)
+    return wrapper
+
+@log_function_data
+def add(x,y):
+    '''Adds two numbers together'''
+    return x+y
+add(3,4)
+
+add.__doc__ #now this will work and come up with 'Adds two numbers together'
+#########################################
+#notice how we have the @wraps
+from functools import wraps
+#wraps preserves a functions metadata when it is decorated
+def my_decorator(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        #do something with fn(*args, **kwargs)
+        pass
+    return wrapper
+
+##########################################
+#More useful ways of using decorators
+##########################################
