c23127902ba27fd1261f58f5ce1c0acf7f8dfc1d
Jamie <jamiecass92@gmail.com>
2020-08-13 10:57:11 +1000

    added to scraping task

--- scraping_task.py
+++ scraping_task.py
@@ -3,12 +3,14 @@ import requests
 from csv import reader
 from csv import writer
 
-site = 'http://quotes.toscrape.com/'
-response = requests.get(site)
 
-print(response.text)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+for i in range(11):      # Number of pages plus one
+    site = 'http://quotes.toscrape.com/'.format(i)
+    r = requests.get(site)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+
 
 
 # --------- testing some stuff out ---------
@@ -23,8 +25,14 @@ new
 davids = [x.find('a') for x in trial]
 davids
 
-page_list = [c['href'] for c in soup.find_all('a')]
-page_list
+def webIterate():
+    base_link = "http://quotes.toscrape.com/"
+    for i in range(11):
+        return f'http://quotes.toscrape.com/page/{i}'
+webIterate()
+
+next_page = soup.find_all(class_ = 'next')
+next_page
 # ------------------------------------------
 
 
@@ -41,21 +49,10 @@ authors
 
 
 # HREF OF LINK
-links = soup.find_all('span')
-type(links)
-dir(links)
-links
-for link in links:
-    url = link.find('a')
-print(url[0])
 project_href = [i['href'] for i in soup.find_all('a', href=True)]
 project_href
 
 keywords = ['author']
-
-for link in project_href:
-    if all(keyword in link for keyword in keywords):
-        print(link)
 author_links = [link for link in project_href if all(keyword in link for keyword in keywords)]
 author_links
 
@@ -68,10 +65,10 @@ print(alb_bio.text)
 al_soup = BeautifulSoup(alb_bio.text, 'html.parser')
 alb_info = []
 alb_dob = al_soup.find(class_ = 'author-born-date')
-print(alb_description)
 alb_info.append(alb_dob.text)
-alb_info
 alb_loc = al_soup.find(class_ = 'author-born-location')
 alb_info.append(alb_loc.text)
 alb_desc = al_soup.find(class_ = 'author-description')
-alb_info.append(alb_desc.text)
+alb_info.append(alb_desc.text.replace('\n', ''))
+
+alb_info
