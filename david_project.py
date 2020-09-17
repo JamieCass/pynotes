43e371d9bccfe66a708f4ee0f97c50de48dbfbc9
JamieCass <jamiecass92@gmail.com>
2020-09-17 17:00:19 +1000

    added more to project, needs work though

--- david_project.py
+++ david_project.py
@@ -3,12 +3,25 @@ from bs4 import BeautifulSoup
 from time import sleep
 import sqlite3
 
-base_url = 'http://books.toscrape.com'
-url = '/index.html'
+############### initial urls ###############
 
-res = requests.get(f'{base_url}{url}')
+base_url = 'http://books.toscrape.com'
+url = 'index.html'
+
+
+res = requests.get(f'{base_url}/{url}')
 soup = BeautifulSoup(res.text, 'html.parser')
+############### list of the book pages ###############
+
+books = soup.find_all('article')
+
+all_books = []
+for book in books:
+    book_link = book.find('a')['href']
+    all_books.append(book_link)
+all_books
 
 ############### go to the next page ###############
 next_btn = soup.find(class_='next')
 url = next_btn.find('a')['href'] if next_btn else None
+url
