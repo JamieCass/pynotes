e61dd945acd825eea270fc4d08b545473c71652c
JamieCass <jamiecass92@gmail.com>
2020-10-12 11:52:21 +1000

    added a fake news file

--- multiporcessing.py
+++ multiporcessing.py
@@ -49,34 +49,11 @@ if __name__ == '__main__':
     for proc in procs:
         proc.join()
 
-
-def doubler(number):
-    """
-    A doubling function that can be used by a process
-    """
-    result = number * 2
-    proc_name = current_process().name
-    print('{0} doubled to {1} by: {2}'.format(
-        number, result, proc_name))
-if __name__ == '__main__':
-    numbers = [5, 10, 15, 20, 25]
-    procs = []
-    proc = Process(target=doubler, args=(5,))
-    for index, number in enumerate(numbers):
-        proc = Process(target=doubler, args=(number,))
-        procs.append(proc)
-        proc.start()
-    proc = Process(target=doubler, name='Test', args=(2,))
-    proc.start()
-    procs.append(proc)
-    for proc in procs:
-        proc.join()
-
 # Using processing pool
-from multiprocessing import Pool
-def doubler(number):
-    return number * 2
-if __name__ == '__main__':
-    numbers = [5, 10, 20]
-    pool = Pool(processes=3)
-    print(pool.map(doubler, numbers))
+# from multiprocessing import Pool
+# def doubler(number):
+#     return number * 2
+# if __name__ == '__main__':
+#     numbers = [5, 10, 20]
+#     pool = Pool(processes=3)
+#     print(pool.map(doubler, numbers))
