f8820fdc47e218ebaeaa9feeb3f3a00d0923a308
Jamie <>
2021-03-23 13:31:35 +1000

    added airflow install help

--- coding_help.py
+++ coding_help.py
@@ -59,6 +59,31 @@ browser.set_window_size(720,1160)
 
 # To preview a md file in atom.. ctrl, shift, m.
 
+##################################################
+# PostgreSQL
+##################################################
+# Start Postgres
+pg_ctl -D /usr/local/var/postgres start
+# Stop Postgres
+pg_ctl -D /usr/local/var/postgres stop
+
+
+##################################################
+# Airflow install
+##################################################
+sudo pip3 install virtualenv
+mkdir airflow
+virtualenv -p python3 jc_env
+source jc_env/bin/activate
+pip install --upgrade pip==20.2.4
+export AIRFLOW_HOME=~/airflow
+AIRFLOW_VERSION=2.0.1
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+echo $PYTHON_VERSION
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+
 
 ##################################################
 # Github help
